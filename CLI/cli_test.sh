4d4741157f5fe96bd85139b274f52671d56d527f
Jan <jan.zgraggen@epfl.ch>
2025-04-30 22:57:14 +0200

    CLI sh files and yaml configs

---
+++ CLI/cli_test.sh
@@ -0,0 +1,15 @@
+morphoclass extract-features \
+    assets/datasets_structured_layer/pyramidal-cells/L5/dataset.csv \
+    apical \
+    image-tmd-rd \
+    output/extract-features/pc-L5/apical/image-tmd-rd/ --force
+
+morphoclass train \
+    --features-dir output/extract-features/pc-L5/apical/image-tmd-rd/ \
+    --model-config training/configs/model-decision-tree.yaml \
+    --splitter-config CLI/configs/splitter-stratified-k-fold.yaml \
+    --checkpoint-dir output/pc-L5-apical-image-tmd-rd-decision-tree/
+
+morphoclass evaluate performance \
+    output/pc-L5-apical-image-tmd-rd-decision-tree/checkpoint.chk \
+    output/evaluation_report.html
