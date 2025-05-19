95e43bf3c6e08d71e617597e14f52dc04b008269
Jan <jan.zgraggen@epfl.ch>
2025-05-19 14:18:35 +0200

    add execution shelscripts for Extract: E and Graph run G

---
+++ runE.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check for two arguments
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <configname>"
+  echo "Example: $0 extract-multi"
+  exit 1
+fi
+
+CONFIG_DIR="src/configs/$1.yaml"
+
+# Run Extraction
+python run_MultiFeatureExtract.py \
+    "$CONFIG_DIR"
+
+
--- src/scripts/exp_multiConcat.sh
+++ src/scripts/exp_multiConcat.sh
@@ -7,11 +7,11 @@ export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export PYTHONPATH=".:$PYTHONPATH"
 morphoclass --verbose train \
-    --features-dir output/multiconcat_features/ \
+    --features-dir output/features/features:kanari18_laylab \
     --model-config src/configs/model-multiconcat.yaml \
     --splitter-config src/configs/splitter-stratified-k-fold.yaml \
-    --checkpoint-dir output/multiconcat_checkpoint/
+    --checkpoint-dir output/results/features:kanari18laylab/ 
 
 morphoclass --verbose evaluate performance \
-    output/multiconcat_checkpoint/checkpoint.chk \
-    output/eval_multiconcat.html
+    output/results/features:multiconcat/checkpoint.chk \
+    output/results/features:multiconcat/eval_multiconcat.html
